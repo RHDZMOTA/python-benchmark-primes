c9633f6549a88f6d433cd62f37b3e191b5a81490
rhdzmota <rhdzmota@gmail.com>
2021-10-25 08:29:49 -0500

    Add new execution options

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 
 function build() {
   case "${1}" in
+    --c-standalone )
+      gcc -o standalonec standalone.c
+      ;;
     --c )
-      gcc -o benchmarkc benchmark.c
+      gcc -c -Wall -Werror -fpic isprime.c
+      gcc -shared -o libisprime.so isprime.o
+      gcc -L${PWD} -Wall -o benchmarkc benchmark.c -lisprime
       ;;
     --py-numba )
       python numba_source.py
+      ;;
+    --py-cython )
+      python setup.py build_ext --inplace
       ;;
   esac
   shift
--- run.sh
+++ run.sh
@@ -1,14 +1,26 @@
 
 function run() {
   case "${1}" in
+    --c-standalone )
+      ./standalonec
+      ;;
     --c )
-      ./benchmarkc
+      LD_LIBRARY_PATH=${PWD} ./benchmarkc
       ;;
     --py-naive )
       python benchmark.py naive_impl is_prime 250001
       ;;
+    --py-ctypes )
+      python benchmark.py ctypes_impl is_prime 250001
+      ;;
     --py-numba )
       python benchmark.py numba_impl is_prime 250001
+      ;;
+    --py-cython-naive )
+      python benchmark.py cython_naive_impl is_prime 250001
+      ;;
+    --py-cython )
+      python benchmark.py cython_impl is_prime 250001
       ;;
   esac
   shift
